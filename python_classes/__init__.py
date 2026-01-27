4442e23c246d37d2b0734c8e99ddc545e7fa32ca
Brandyn Britton <brandynbb96@gmail.com>
2026-01-27 20:45:19 +0800

    feat(UI): Add Quick Generate Tree operator and UI panel

--- python_classes/__init__.py
+++ python_classes/__init__.py
@@ -1,11 +1,15 @@
 from . import operators
 from . import nodes
+from . import panels
+
 
 def register():
     operators.register()
     nodes.register()
+    panels.register()
 
 
 def unregister():
     operators.unregister()
     nodes.unregister()
+    panels.unregister()
