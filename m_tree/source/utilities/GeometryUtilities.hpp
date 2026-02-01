64df05ee85d1a6b3115b83fe536a76782d10bf89
Brandyn Britton <brandynbb96@gmail.com>
2026-02-01 13:12:30 +0800

    feat: fallback for PI

--- m_tree/source/utilities/GeometryUtilities.hpp
+++ m_tree/source/utilities/GeometryUtilities.hpp
@@ -2,11 +2,8 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <cmath>
+#include <numbers>
 #include <vector>
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
 
 namespace Mtree
 {
